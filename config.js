544d3d9ae4a94e89649d272b6d12e1357245265f
tensaye-dior <chakaangel55@gmail.com>
2026-09-09 22:21:45 +1000

    Add config.js for Candle Heaven storefront

---
+++ config.js
@@ -0,0 +1,8 @@
+window.CANDLE_CONFIG = {
+  businessName: "Candle Heaven",
+  phone: "61423727047",
+  facebook: "https://www.facebook.com/share/1PsRgtyCiP/?mibextid=wwXIfr",
+  instagram: "https://www.instagram.com/",
+  currency: "AUD",
+  stripePaymentLink: ""
+};
