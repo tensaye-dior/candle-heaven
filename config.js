ba9c5d1a5a1da6e7fbf4534b11575c427d69d5d9
tensaye-dior <chakaangel55@gmail.com>
2026-09-10 15:33:01 +1000

    Update Candle Heaven contact and Square checkout config

--- config.js
+++ config.js
@@ -1,8 +1,10 @@
 window.CANDLE_CONFIG = {
   businessName: "Candle Heaven",
-  phone: "61423727047",
+  phone: "61466846785",
+  email: "candleheaven609@gmail.com",
   facebook: "https://www.facebook.com/share/1PsRgtyCiP/?mibextid=wwXIfr",
   instagram: "https://www.instagram.com/",
   currency: "AUD",
+  squarePaymentLink: "",
   stripePaymentLink: ""
 };
